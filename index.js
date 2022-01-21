58cc9ff7b94fc54328fa44f01191a34f668e9908
Greenstar047 <98154987+Greenstar047@users.noreply.github.com>
2022-01-21 12:26:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client(
     { intents:["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "DIRECT_MESSAGE_TYPING"] }
 )
 
-client.login("OTMzOTkzNTcwNjY5NTY4MDEw.YepnTA.D4ZJkHEOMf2McOUtEko1VW2-42Q")
+client.login(process.env.token)
 
 client.on("ready", () => {
     console.log("bot ONLINE")
